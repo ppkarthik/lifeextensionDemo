4fd9b4ba857027b7bdc8ac2a518b3d65d2ec153c
ppkarthik <kajanardhanan@paypal.com>
2020-01-12 23:39:20 -0800

    Update thankyou.php

--- thankyou.php
+++ thankyou.php
@@ -1,5 +1,6 @@
 <html>
 <body>
+<input type="hidden" id="nonce" name="payment_method_nonce" />
 <script type="text/javascript">
 
 function getQueryVariable(variable)
@@ -13,7 +14,39 @@ function getQueryVariable(variable)
        return(false);
 }
 console.log(getQueryVariable("nonce"));
+document.querySelector('#nonce').value = getQueryVariable("nonce");
 </script>
- 
+
+       
+<?php
+require 'braintree-php-4.5.0/lib/Braintree.php';
+$gateway = new Braintree_Gateway([
+    'environment' => 'sandbox',
+    'merchantId' => 'hks7325w6hqpmygy',
+    'publicKey' => '4r34k2f6w522kbmv',
+    'privateKey' => '487037d54eef8df77798d8efc32bd3fd'
+]);
+
+if (isset($_GET['nonce'])) {
+    $nonceFromClient = $_GET['nonce'];
+
+    $result = $gateway->transaction()->sale([
+        'amount' => '8.78',
+        'paymentMethodNonce' => $nonceFromClient,
+        'options' => [
+          'submitForSettlement' => true
+        ],
+    ]);
+
+    header('Content-type: application/json');
+    print json_encode($result);
+    exit;
+}
+       
+       
+       
+?>       
+       
+
 </body>
 </html> 

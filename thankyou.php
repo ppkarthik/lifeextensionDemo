386d8a65e1623287500254d638c8d170ca700178
ppkarthik <kajanardhanan@paypal.com>
2020-01-12 23:30:35 -0800

    Update thankyou.php

--- thankyou.php
+++ thankyou.php
@@ -1,8 +1,19 @@
 <html>
- <body>
+<body>
+<script type="text/javascript">
 
- Hello!<br>
- Your nonce is <?php echo   . htmlspecialchars($_GET["nonce"]) . '!'; ?>.
-
- </body>
- </html> 
+function getQueryVariable(variable)
+{
+       var query = window.location.search.substring(1);
+       var vars = query.split("&");
+       for (var i=0;i<vars.length;i++) {
+               var pair = vars[i].split("=");
+               if(pair[0] == variable){return pair[1];}
+       }
+       return(false);
+}
+console.log(getQueryVariable("nonce"));
+</script>
+ 
+</body>
+</html> 
